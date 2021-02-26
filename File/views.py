32039fdf8c0c1e7ef4c7643f55b71a80207e1f04
MatinMat8 <matinmat8.mat@gmail.com>
2021-02-26 09:45:00 +0000

    Build Send Email When a request was made to build a site.

--- File/views.py
+++ File/views.py
@@ -53,6 +53,18 @@ def Upload(request):
             new_item.user = request.user
             new_item.save()
             messages.success(request, 'your form upload successfully!!')
+            cd = form.cleaned_data
+            subject = f"{cd['name']} project for this user {new_item.user} want you build website for him" \
+                      f"{cd['name']} and this is BudgetAmount {cd['BudgetAmount']}"
+            message = f"this is project name{Upload_File.name} for this user:{new_item.user} this is email for this " \
+                      f"project and user{cd['email']} and this is url for example project{cd['example']}"\
+                      f"this is file for this project{cd['File']} and this is phone number for user" \
+                      f"{cd['phone_number']}"
+            send_mail(subject,
+                      message,
+                      'sitemat8@gmail.com',
+                      ['sitemat8@example.com'])
+
             return redirect('article:index')
     else:
         form = Upload_file()
