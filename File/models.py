e0c2b267c4339ed86783ea4e27a2997e032c4462
MatinMat8 <matinmat8.mat@gmail.com>
2021-05-10 18:43:00 +0430

    add a help_text in email field

--- File/models.py
+++ File/models.py
@@ -10,7 +10,7 @@ from .validators import validate_file_extension
 class Upload_File(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=60)
-    email = models.EmailField(blank=False, help_text='Please describe your project')
+    email = models.EmailField(blank=False)
     subject_project = models.TextField(blank=True)
     example = models.URLField(max_length=250, blank=True, null=True)
     phone_number = PhoneNumberField(blank=True, null=True)
